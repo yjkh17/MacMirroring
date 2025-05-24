6e61e93803c2e452136a74f33bb8ae0bcc48677a
yjkh17 <yj.kh17@gmail.com>
2025-05-24 20:35:47 +0300

    Initial Commit

---
+++ MacMirroring/MacMirroringApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMirroringApp.swift
+//  MacMirroring
+//
+//  Created by Yousef Jawdat on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MacMirroringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
